ecaa3f47946c31602999c0cbe0ac5005171e3750
Nick Brassel <nick@tzarc.org>
2019-12-04 11:21:05 +1100

    Retarget 19.07.0-rc2.

--- build-image.sh
+++ build-image.sh
@@ -5,8 +5,8 @@ source common.bashinc.sh
 
 # Overrides...
 export target_dir="$script_dir/img"
-export default_version="18.06.5"
-declare -a supported_versions=( master 18.06.5 )
+export default_version="19.07.0-rc2"
+declare -a supported_versions=( master 19.07.0-rc2 )
 
 # No args => help
 if [ "${#script_args[@]}" -eq "0" ] ; then
